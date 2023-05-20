77e6ea35eadad476d8328136084f0c9d1d163dc2
Disha Gupta <87701181+dishagupta3251@users.noreply.github.com>
2023-05-20 10:09:37 +0530

    Time: 4 ms (96.35%), Space: 47.8 MB (14.62%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -14,27 +14,15 @@ class Solution {
            if(ruleKey.equals("type"))
             {
                count=cal(items,0,ruleValue);
-            //     for(int i=0;i<items.size();i++)
-            // {   
-            //     if(items.get(i).get(0).equals(ruleValue))
-            //         c++;
-            // }
+            
         }
         if(ruleKey.equals("color"))
             {count=cal(items,1,ruleValue);
-            //     for(int i=0;i<items.size();i++)
-            // {   
-            //     if(items.get(i).get(1).equals(ruleValue))
-            //         c++;
-            // }
+           
         }
         if(ruleKey.equals("name"))
             {count=cal(items,2,ruleValue);
-            //     for(int i=0;i<items.size();i++)
-            // {   
-            //     if(items.get(i).get(2).equals(ruleValue))
-            //         c++;
-            // }
+            
         }
         
         return count;
