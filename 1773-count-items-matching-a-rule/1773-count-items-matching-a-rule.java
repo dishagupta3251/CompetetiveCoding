36ec16249413aa6b71bc102bd9be6cddb9640525
Disha Gupta <87701181+dishagupta3251@users.noreply.github.com>
2023-05-20 10:06:40 +0530

    Time: 4 ms (96.35%), Space: 47.3 MB (86.80%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,31 +1,42 @@
 class Solution {
+    int cal(List<List<String>> products,int place,String value)
+    {int c=0;
+        for(int i=0;i<products.size();i++)
+            {   
+                if(products.get(i).get(place).equals(value))
+                    c++;
+            }
+     return c;
+        
+    }
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int c=0;
+        int count=0;
            if(ruleKey.equals("type"))
             {
-                for(int i=0;i<items.size();i++)
-            {   
-                if(items.get(i).get(0).equals(ruleValue))
-                    c++;
-            }
+               count=cal(items,0,ruleValue);
+            //     for(int i=0;i<items.size();i++)
+            // {   
+            //     if(items.get(i).get(0).equals(ruleValue))
+            //         c++;
+            // }
         }
         if(ruleKey.equals("color"))
-            {
-                for(int i=0;i<items.size();i++)
-            {   
-                if(items.get(i).get(1).equals(ruleValue))
-                    c++;
-            }
+            {count=cal(items,1,ruleValue);
+            //     for(int i=0;i<items.size();i++)
+            // {   
+            //     if(items.get(i).get(1).equals(ruleValue))
+            //         c++;
+            // }
         }
         if(ruleKey.equals("name"))
-            {
-                for(int i=0;i<items.size();i++)
-            {   
-                if(items.get(i).get(2).equals(ruleValue))
-                    c++;
-            }
+            {count=cal(items,2,ruleValue);
+            //     for(int i=0;i<items.size();i++)
+            // {   
+            //     if(items.get(i).get(2).equals(ruleValue))
+            //         c++;
+            // }
         }
         
-        return c;
+        return count;
     }
 }
