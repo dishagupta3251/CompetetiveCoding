b2d2783b089d976575fbf8ad360a0fa63c251c11
Disha Gupta <87701181+dishagupta3251@users.noreply.github.com>
2023-07-23 21:27:44 +0530

    Time: 7 ms (7.37%), Space: 41.2 MB (12.65%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        StringBuilder sb=new StringBuilder(); String res="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(Character.isAlphabetic(s.charAt(i)))
+                sb.append(s.charAt(i));
+        }
+        sb.reverse();
+        int k=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(Character.isAlphabetic(s.charAt(i)))                
+               { 
+                if(Character.isAlphabetic(s.charAt(i)))
+                   { res=res+sb.charAt(k);
+                k++;}
+            }
+                else
+                    res=res+s.charAt(i);
+            
+        }
+        
+        return res;
+    }
+}
