78ae00f055ead0351441eb0b3acb6aa4b07aa73f
Disha Gupta <87701181+dishagupta3251@users.noreply.github.com>
2023-07-24 23:07:09 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (76.76%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,13 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        String res="";
-        for(int i=0;i<s.length();i++)
-        {
-            if(Character.isUpperCase(s.charAt(i)))
-            res+=(char)((s.charAt(i)-'A')+'a');
-            else
-                res+=s.charAt(i);
-        }
-        return res;
+      return s.toLowerCase();
     }
 }
