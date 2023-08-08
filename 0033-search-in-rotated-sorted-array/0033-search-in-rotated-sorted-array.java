445e1ac61bcbde4e2b078d7ad7e8f98c9b221230
Disha Gupta <87701181+dishagupta3251@users.noreply.github.com>
2023-08-08 16:38:38 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (92.66%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+      int search(int arr[], int l, int h, int key)
+    {
+        if (l > h)
+            return -1;
+ 
+        int mid = (l + h) / 2;
+        if (arr[mid] == key)
+            return mid;
+ 
+        
+        if (arr[l] <= arr[mid]) {
+            
+            if (key >= arr[l] && key <= arr[mid])
+                return search(arr, l, mid - 1, key);
+            
+            return search(arr, mid + 1, h, key);
+        }
+             if (key >= arr[mid] && key <= arr[h])
+            return search(arr, mid + 1, h, key);
+ 
+        return search(arr, l, mid - 1, key);
+        }
+      
+    public int search(int[] nums, int target) {
+        int res=search(nums,0,nums.length-1,target);
+        return res;
+       }
+        
+    }
