644ac7ace49dbb4912854f4e2468927dcf2ed7f4
Disha Gupta <87701181+dishagupta3251@users.noreply.github.com>
2023-08-10 15:54:19 +0530

    Time: 6 ms (12.10%), Space: 40.6 MB (44.41%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,22 +1,35 @@
 import java.util.*;
 class Solution {
     public int countPoints(String rings) {
-        HashMap<Character,String> map=new HashMap<>();
-            for(int i=1;i<rings.length();i+=2)
-            {
-                if(map.containsKey(rings.charAt(i)))
-                {
-                    map.put(rings.charAt(i),map.get(rings.charAt(i))+rings.charAt(i-1));
-                }
-                else
-                    map.put(rings.charAt(i),""+rings.charAt(i-1));
-            }
-        int c=0;
-        for(Map.Entry<Character,String> hm :map.entrySet())
+        // HashMap<Character,String> map=new HashMap<>();
+        //     for(int i=1;i<rings.length();i+=2)
+        //     {
+        //         if(map.containsKey(rings.charAt(i)))
+        //         {
+        //             map.put(rings.charAt(i),map.get(rings.charAt(i))+rings.charAt(i-1));
+        //         }
+        //         else
+        //             map.put(rings.charAt(i),""+rings.charAt(i-1));
+        //     }
+        // int c=0;
+        // for(Map.Entry<Character,String> hm :map.entrySet())
+        // {
+        //     if(hm.getValue().contains("B")&&hm.getValue().contains("G")&&hm.getValue().contains("R"))
+        //         c++;
+        // }
+        String ar[]=new String[10];int c=0;
+for(int i=1;i<rings.length();i=i+2)
+{
+    int index=(int)rings.charAt(i)-'0';
+    ar[index]=ar[index]+rings.charAt(i-1);
+}
+        for(int i=0;i<ar.length;i++)
         {
-            if(hm.getValue().contains("B")&&hm.getValue().contains("G")&&hm.getValue().contains("R"))
-                c++;
+            if(ar[i]!=null)
+            {if(ar[i].contains("B")&&ar[i].contains("G")&&ar[i].contains("R"))
+                c++;}
         }
         return c;
     }
 }
+
