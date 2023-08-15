edb61cc4f562dc5d4655f4846cc279a6143a2e42
Disha Gupta <87701181+dishagupta3251@users.noreply.github.com>
2023-08-15 17:06:13 +0530

    Time: 13 ms (30.16%), Space: 46.2 MB (88.82%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ar=new ArrayList<Integer>();
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.containsKey(nums[i]))
+                map.put(nums[i],map.get(nums[i])+1);
+            else
+                map.put(nums[i],1);
+        }
+        for(Map.Entry<Integer,Integer> hm:map.entrySet())
+        {
+            if(hm.getValue()>(nums.length/3))
+                ar.add(hm.getKey());
+        }
+        return ar;
+    }
+}
